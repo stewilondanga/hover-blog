9e706fab76151a1b5c8c2fade0fcfc5658b8a66a
Stewart ilondanga <stilondanga@gmail.com>
2018-11-14 11:20:43 +0300

    add css/styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
@@ -6,3 +6,41 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+//It's messy, don't look at it lol
+/*
+function resize() {
+	var actualInnerWidth = document.body.clientWidth;
+	document.getElementById("B").style.width = actualInnerWidth + "px";
+}
+
+resize();
+window.onresize = function() {
+	resize();
+};
+
+/*** *** *** *** ***/
+/*
+$('.A').mousemove(function(e){
+	var offs = $(this).offset(),
+			p    = {x:offs.left, y:offs.top},
+			mPos = {x:e.pageX, y:e.pageY},
+			x    = mPos.x - p.x - ($('.insight').width()/2),
+			y    = mPos.y - p.y - ($('.insight').height()/2);
+	$('.insight', this).css({left:x, top:y});
+	$('.B', this).css({left:-x, top:-y});
+});
+
+/*** *** *** *** ***/
+
+/*$(document).ready(function(){
+	$('img').each(function(){
+		var src = $(this).attr('src');
+		$(this).css('background-image', 'url(' + src + ')');
+	});
+});
+
+$(window).on("scroll", function() {
+	var yScroll = $(this).scrollTop();
+	$(".plax").css({"background-position": '50% ' + ((yScroll / 10) + 0) + '%'});
+});
